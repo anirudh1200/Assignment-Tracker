ce921ae011eeac7cc8764e462c808b3f50a4c961
Anirudh Bhat <anirudh.b.123@gmail.com>
2018-12-01 22:34:00 +0530

    made minor changes for production build

--- client/src/store/reducers/SubmissionReducer.js
+++ client/src/store/reducers/SubmissionReducer.js
@@ -9,7 +9,10 @@ if(localStorage.getItem('data')){
 const initState = {
     submissions: savedData,
     loading: false,
-    socket: socketIOClient("http://localhost:5000")
+    //for local build
+    // socket: socketIOClient("http://localhost:5000")
+    //for production build
+    socket: socketIOClient("https://damp-refuge-65272.herokuapp.com/")
 }
 
 const SubmissionReducer = (state = initState, action) => {
